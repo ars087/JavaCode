0c182deb0ba0cbd5f1461b0204256dc331ed43c0
ars087 <ars5885@gmail.com>
2024-11-29 14:49:28 +0400

    Tast Maven Library

---
+++ GeometryUtils/src/main/java/com/example/geometry/GeometryUtils.java
@@ -0,0 +1,10 @@
+package com.example.geometry;
+
+
+public class GeometryUtils {
+    public  double convertToSquareMeters(double squareFeet) {
+
+        return squareFeet * 1000;
+    }
+
+}
---
+++ ThreeDimensionalShapes/src/main/java/com/example/geometry/Main.java
@@ -0,0 +1,7 @@
+package com.example.geometry;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
