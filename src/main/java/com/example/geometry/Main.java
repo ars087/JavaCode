52fda8725deb39f6ad89a4a30127d11d46c58745
ars087 <ars5885@gmail.com>
2024-11-29 15:32:28 +0400

    Stream API Task-one- NumberGeneration

---
+++ src/main/java/com/example/geometry/Main.java
@@ -0,0 +1,39 @@
+package com.example.geometry;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        List<Order> orders = List.of(
+            new Order("Laptop", 1200.0),
+            new Order("Smartphone", 800.0),
+            new Order("Laptop", 1500.0),
+            new Order("Tablet", 500.0),
+            new Order("Smartphone", 900.0)
+        );
+
+
+        Map<String, Double> totalCostByProduct = orders.stream()
+            .collect(Collectors.groupingBy(Order::getProduct,
+                Collectors.summingDouble(Order::getCost)));
+
+
+        List<Map.Entry<String, Double>> topProducts = totalCostByProduct.entrySet().stream()
+            .sorted(Map.Entry.<String, Double>comparingByValue().reversed())
+            .limit(3)
+            .collect(Collectors.toList());
+
+
+        System.out.println("Три самых дорогих продукта и их общая стоимость:");
+        for (Map.Entry<String, Double> entry : topProducts) {
+            System.out.printf("Продукт: %s, Общая стоимость: %.2f%n", entry.getKey(), entry.getValue());
+
+
+        }
+    }
+}
